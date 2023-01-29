36fc42bf40708fe1113c169ed9657e1899382081
JJang95 <jojang1994@gmail.com>
2023-01-29 16:59:32 -0500

    Finished Q2

--- Q2/main.js
+++ Q2/main.js
@@ -3,3 +3,31 @@ function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("productTable").deleteRow(i);
 }
+
+function addProduct() {
+    var table = document.getElementById("productTable")
+    var productName = document.getElementById("productName").value;
+    var productCategory = document.getElementById("productCategory").value;
+    var productPrice = document.getElementById("productPrice").value;
+
+    var html = `
+    
+    <tr>
+    
+        <td>${productName}</td>
+        
+        <td>${productCategory}</td>
+        
+        <td>$${productPrice}</td>
+        
+        <td>
+            <input type="button" value="Delete" onclick="deleteRow(this)">
+        </td>
+    
+    </tr>
+    
+    `;
+
+    table.innerHTML += html;
+
+}
